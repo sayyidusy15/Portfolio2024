db1cbc7c8528f81e96ec33179420d1df9fab4288
Sayyidusy syauqi Al Ghiffari <88390553+Sayyidusy@users.noreply.github.com>
2024-08-27 04:27:11 +0700

    ganti skills

--- src/components/layouts/resume/index.tsx
+++ src/components/layouts/resume/index.tsx
@@ -93,18 +93,17 @@ export default function Resume() {
 
       <section className="skill">
         <h3 className="h3 skills-title">My skills</h3>
-
         <ul className="skills-list content-card">
           <li className="skills-item">
             <div className="title-wrapper">
               <h5 className="h5">Web design</h5>
-              <data value="80">80%</data>
+              <data value="80">70%</data>
             </div>
 
             <div className="skill-progress-bg">
               <div
                 className="skill-progress-fill"
-                style={{ width: "80%" }}
+                style={{ width: "70%" }}
               ></div>
             </div>
           </li>
@@ -139,18 +138,20 @@ export default function Resume() {
 
           <li className="skills-item">
             <div className="title-wrapper">
-              <h5 className="h5">WordPress</h5>
+              <h5 className="h5">Front End Dev</h5>
               <data value="50">50%</data>
             </div>
 
             <div className="skill-progress-bg">
               <div
                 className="skill-progress-fill"
-                style={{ width: "80%" }}
+                style={{ width: "50%" }}
               ></div>
             </div>
           </li>
         </ul>
+
+
       </section>
     </article>
   );
